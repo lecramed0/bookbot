858f1d86273e963edb8cff787575ad0afbd21622
lecramed <lecramed@gmail.com>
2026-01-05 08:49:11 -0330

    added CLI arguments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import sys
 from stats import wordcount, charactercount, character_report
 
 # constants
-
-frank = "books/frankenstein.txt"
+#frank = "books/frankenstein.txt"
 
 # functions
 
@@ -12,13 +12,18 @@ frank = "books/frankenstein.txt"
 #        return file_contents
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_book = sys.argv[1]
     #filepath = input("provide a file path")
     #book_text = get_book_text(filepath)
-    book_wordcount = wordcount(frank)
-    book_characters = charactercount(frank)
+    book_wordcount = wordcount(path_to_book)
+    book_characters = charactercount(path_to_book)
     book_character_report = character_report(book_characters)
     print(f'''============ BOOKBOT ============
-Analyzing book found at {frank}...
+Analyzing book found at {path_to_book}...
 ----------- Word Count ----------
 Found {book_wordcount} total words
 --------- Character Count -------''')
